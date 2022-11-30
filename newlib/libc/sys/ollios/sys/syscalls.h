5c660ddc193d8894101c8a217b0f371fb966e9a3
Olivier de Schaetzen <de.schaetzen.olivier@gmail.com>
2022-11-30 21:28:02 +0100

    implemented get cmos time syscall

--- newlib/libc/sys/ollios/sys/syscalls.h
+++ newlib/libc/sys/ollios/sys/syscalls.h
@@ -24,5 +24,18 @@
 #define SYSINT_READDIR 21
 #define SYSINT_GETWD  22
 #define SYSINT_USLEEP 23
-
+#define SYSINT_GET_CMOS_UTC 24
 #endif
+
+
+typedef struct _cmos_time {
+    uint8_t seconds;
+    uint8_t minutes;
+    uint8_t hours;
+    uint8_t day;
+    uint8_t month;
+    uint8_t year;
+} cmos_time_t;
+
+
+int _get_cmos_utc(cmos_time_t *ct):
